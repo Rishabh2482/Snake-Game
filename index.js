40bbb9c549d8ad7c09956743afccf8703c7ad2f0
Rishabh2482 <rishabh2002chauhan@gmail.com>
2025-06-23 23:23:04 +0530

    Initiated the project and created the starting html and css part

---
+++ index.js
@@ -0,0 +1,6 @@
+const board = document.getElementById('gameBoard');
+const scoreElement = document.getElementById('score');
+const controls = document.getElementById('controls');
+const overlay = document.getElementById('overlay');
+const gameOverMessgae = document.getElementById('gameOverMsg');
+const playButton = document.getElementById('playButton');
